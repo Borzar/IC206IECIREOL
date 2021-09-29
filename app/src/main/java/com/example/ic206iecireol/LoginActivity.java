a2fb0a453f12c33e701f6be9aa2c67256bed7be7
Boris <boris.zlobos@gmail.com>
2021-09-28 22:53:41 -0300

    DatePickerFragment y validacion campos del login

--- app/src/main/java/com/example/ic206iecireol/LoginActivity.java
+++ app/src/main/java/com/example/ic206iecireol/LoginActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin, btnRegister;
+    private TextInputLayout tilUser, tilPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +26,43 @@ public class LoginActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.activity_login_btn_login);
         btnRegister = findViewById(R.id.activity_login_btn_register);
+        tilUser = findViewById(R.id.activity_login_field_user);
+        tilPassword = findViewById(R.id.activity_login_field_password);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "Iniciando Sesión", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(view.getContext(), MainActivity.class);
-                startActivity(i);
-                finish();
+
+                String user = tilUser.getEditText().getText().toString();
+                String password = tilPassword.getEditText().getText().toString();
+
+                boolean userValid = !user.isEmpty();
+                boolean passwordValid = !password.isEmpty();
+
+                if (!userValid) {
+                     tilUser.setError("El usuario esta vacio");
+                } else {
+                    tilUser.setError(null);
+                    tilUser.setErrorEnabled(false);
+                }
+
+                if (!passwordValid) {
+                    tilPassword.setError("Campo requerido");
+                } else {
+                    tilPassword.setError(null);
+                    tilPassword.setErrorEnabled(false);
+                }
+
+                if (password.equals("1234") && userValid && passwordValid) {
+                   Toast.makeText(view.getContext(), String.format("Bienvenido %s", user), Toast.LENGTH_SHORT).show();
+                   Intent i = new Intent(view.getContext(), MainActivity.class);
+                   startActivity(i);
+                   finish();
+                } else {
+                   Toast.makeText(view.getContext(), String.format("La contraseña es incorrecta", user), Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
--- app/src/main/java/com/example/ic206iecireol/RegisterActivity.java
+++ app/src/main/java/com/example/ic206iecireol/RegisterActivity.java
@@ -6,8 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.ic206iecireol.ui.DatePickerFragment;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Date;
+
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextInputLayout tilBirthday;
     private Button btnRegister, btnLogin;
 
     @Override
@@ -15,8 +21,13 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        tilBirthday = findViewById(R.id.activity_register_field_birth_date);
         btnRegister = findViewById(R.id.activity_register_btn_register);
         btnLogin = findViewById(R.id.activity_register_btn_login);
+
+        tilBirthday.getEditText().setOnClickListener(view -> {
+            DatePickerFragment.showDatePickerDialog(this, tilBirthday, new Date());
+        });
 
         btnLogin.setOnClickListener(view -> {
             Intent i = new Intent(view.getContext(), LoginActivity.class);
